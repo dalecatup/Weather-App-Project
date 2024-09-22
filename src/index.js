50cbae43763d3623b4f518217cb737933825fc9e
Marina Reggiani <dalecatup@gmail.com>
2024-09-22 08:52:23 +0100

    Updated weather data functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let city = response.data.city;
+  let descriptionSentence = response.data.condition.description;
+  let humidityInfo = `${response.data.temperature.humidity}%`;
+  let windSpeedInfo = `${response.data.wind.speed}km/h`;
+  let timestamp = response.data.time;
+
+  updateDateTime(timestamp);
+
+  let numberElement = document.querySelector(".current-temp-number");
+  numberElement.innerHTML = `${temperature}`;
+
+  let cityName = document.querySelector(".current-city");
+  cityName.innerHTML = city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = descriptionSentence;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = humidityInfo;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = windSpeedInfo;
+}
+
 function formatDate(date) {
   let days = [
     "Sunday",
@@ -10,7 +36,6 @@ function formatDate(date) {
   ];
 
   let currentDay = days[date.getDay()];
-  let currentDate = date.getDate();
 
   let hours = date.getHours();
   if (hours < 10) {
@@ -25,25 +50,14 @@ function formatDate(date) {
   return `${currentDay} ${hours}:${minutes}`;
 }
 
-function updateDateTime() {
-  let now = new Date();
+function updateDateTime(timestamp) {
+  let now = new Date(timestamp * 1000);
   let formattedDate = formatDate(now);
 
-  let dateTimeElement = document.querySelector(".current-date-time");
+  let dateTimeElement = document.querySelector("#current-date-time");
   dateTimeElement.textContent = formattedDate;
 }
-updateDateTime();
-
-function displayTemperature(response) {
-  let temperature = Math.round(response.data.temperature.current);
-  let city = response.data.city;
-
-  let numberElement = document.querySelector(".current-temp-number");
-  numberElement.innerHTML = `${temperature}`;
-
-  let cityName = document.querySelector(".current-city");
-  cityName.innerHTML = city;
-}
+//updateDateTime();
 
 function updateCity(event) {
   event.preventDefault();
@@ -53,5 +67,16 @@ function updateCity(event) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
+
 let cityForm = document.querySelector("#city-search-form");
 cityForm.addEventListener("submit", updateCity);
+
+function defaultCity() {
+  let apiKey = "2dtdb4c6b36b2bd5c58d04bf0o41ae00";
+  let city = "London";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+defaultCity();
